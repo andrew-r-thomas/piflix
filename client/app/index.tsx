d922e76ee9a429acc1c10f47eb93ddfd3c1c47d5
Andrew Thomas <andrew@andrewrthomas.com>
2024-12-17 18:22:04 -0800

    basic end to end working

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,9 +1,15 @@
 import { Pressable, View, Text } from "react-native";
 
 export default function Home() {
+	const simpleCall = async () => {
+		console.log("called server");
+		const resp = await fetch("http://192.168.50.68:8080");
+		const text = await resp.text();
+		console.log(`server responded with: ${text}`);
+	}
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Pressable style={{ backgroundColor: "green", borderRadius: 2, padding: 8 }} onPress={() => console.log("called server!")}>
+			<Pressable style={{ backgroundColor: "green", borderRadius: 2, padding: 8 }} onPress={simpleCall}>
 				<Text style={{ color: "white" }}>
 					Call Server
 				</Text>
